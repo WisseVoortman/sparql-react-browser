218bd4ddfa323689df0e76dde9efb43c7bc1b239
dion.koolhaas@duo.nl <dion.koolhaas@duo.nl>
2020-04-15 19:35:36 +0200

    set datasource plugin gemaakt

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap';
+import {jQuery as $} from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'popper.js'
+
+function Datasource(props) {
+  return <a class="dropdown-item" href="#" onClick={props.onClick}>
+          {props.name}
+         </a>
+}
+
+class DatasourcePlugin extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        currentDatasource: 0,
+        datasources: [
+          {
+            name: 'Onderwijsregistratie',
+            endpoint: 'http://localhost:8080/rdf4j-workbench/repositories/rio/query'
+          },
+          {
+            name: 'Basisregistraties',
+            endpoint: 'https://data.pdok.nl/sparql'
+          },
+          {
+            name: 'Onderwijsinspectie',
+            endpoint: 'http://localhost:8080/rdf4j-workbench/repositories/ivho/query'
+          }
+        ]
+      };
+    }
+  handleClick(key) {
+    this.setState({
+      currentDatasource: key,
+      datasources: this.state.datasources
+    });
+  }
+  render() {
+    return (
+      <div className="setDataSourceDropdown">
+        <div class="dropdown show">
+          <p>{this.state.datasources[this.state.currentDatasource].name}</p>
+          <a class="btn btn-secondary dropdown-toggle" href="#" role="button"
+          id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">Specificeer bron</a>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            {
+              this.state.datasources.map((item, key) =>
+                <Datasource
+                  key={key}
+                  onClick={() => this.handleClick(key)}
+                  name={item.name}
+                  />
+              //<a class="dropdown-item" href="#" onClick={this.handleClick({key})}>{item.name}</a>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DatasourcePlugin />
   </React.StrictMode>,
   document.getElementById('root')
 );
