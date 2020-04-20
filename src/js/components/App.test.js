f370327f725c15ca23d6a2fddfecf6b2911235cb
dion.koolhaas@duo.nl <dion.koolhaas@duo.nl>
2020-04-20 19:32:49 +0200

    App component test toegevoegd

---
+++ src/js/components/App.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import App from './App';
+import React from 'react';
+
+jest.mock('./DataSourceDropdown', () => 'p');
+
+describe('App component', () => {
+  it('should render component', ()=> {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
