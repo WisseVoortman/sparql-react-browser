0a0ebe70714d602b64bdf336a04302f04046d308
dion.koolhaas@duo.nl <dion.koolhaas@duo.nl>
2020-06-06 11:50:56 +0200

    datasource reducer in own file

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,40 +1,7 @@
-import { SET_CURRENT_DATASOURCE } from '../constants/action-types';
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import sparqlReducer from './sparqlReducer'
-
-const initialConnectionState = {
-    currentDatasource: 0,
-    datasources: [
-      {
-        name: 'Onderwijsregistratie',
-        endpoint: 'http://localhost:8080/rdf4j-workbench/repositories/rio/query',
-      },
-      {
-        name: 'Basisregistraties',
-        endpoint: 'https://data.pdok.nl/sparql',
-      },
-      {
-        name: 'Onderwijsinspectie',
-        endpoint: 'http://localhost:8080/rdf4j-workbench/repositories/ivho/query',
-      },
-      {
-        name: 'Kennisnet',
-        endpoint: 'http://localhost:8080/rdf4j-workbench/repositories/kennisnet/query',
-      },
-    ],
-  };
-
-export const datasourceReducer = (state = initialConnectionState, action) => {
-  let newState = Object.assign({}, state);
-  switch (action.type) {
-    case SET_CURRENT_DATASOURCE:
-      newState.currentDatasource = action.currentDatasource;
-      return newState;
-    default:
-      return state;
-  }
-};
+import sparqlReducer from './sparqlReducer';
+import datasourceReducer from './datasourceReducer';
 
 const reducersCombined = {
   connection: datasourceReducer,
