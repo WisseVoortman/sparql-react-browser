ea2163c404bcc4bc31d964f78d5669e1dd3aada9
dion.koolhaas@duo.nl <dion.koolhaas@duo.nl>
2020-06-25 17:55:37 +0200

    deltaRdfGraph added to state

--- src/js/reducers/sparqlReducer.js
+++ src/js/reducers/sparqlReducer.js
@@ -15,6 +15,14 @@ const initialSparqlState = {
 
       //{"source": ... , "target": ... , "property" : ... },
     ],
+    //deltaRdfMap defines which nodes and links are added
+    //compared to the previous state
+    deltaRdfGrap: {
+      nodes: [
+      ],
+      links: [
+      ],
+    },
   },
 };
 
@@ -33,25 +41,31 @@ export const sparqlReducer = (state=initialSparqlState, action) => {
   }
 
   let rdfGraph;
+  let deltaRdfGrap = {
+    nodes: [],
+    links: [],
+  };
   switch (action.type) {
     case SPARQL_GET_URI_FROM_LABEL_FULFILLED:
       rdfGraph = cloneGraph(state.rdfGraph);
       action.payload.data.results.bindings.forEach(
-        (result) => addTripleToGraph(rdfGraph, action.payload.config.url, result.subject.value, null, null)
+        (result) => addTripleToGraph(rdfGraph, deltaRdfGrap, action.payload.config.url, result.subject.value, null, null)
       );
       return {
           ...state,
           rdfGraph,
+          deltaRdfGrap,
           pendingRequests: state.pendingRequests - 1,
         };
     case SPARQL_GET_TRIPLES_FROM_URI_FULFILLED:
       rdfGraph = cloneGraph(state.rdfGraph);
       action.payload.data.results.bindings.forEach(
-        (result) => addTripleToGraph(rdfGraph, action.payload.config.url, action.meta.subject, result.property.value, result.object.value)
+        (result) => addTripleToGraph(rdfGraph, deltaRdfGrap, action.payload.config.url, action.meta.subject, result.property.value, result.object.value)
       );
       return {
           ...state,
           rdfGraph,
+          deltaRdfGrap,
           pendingRequests: state.pendingRequests - 1,
         };
     default:
@@ -64,43 +78,35 @@ const cloneGraph = (rdfGraph) => ({
   links: rdfGraph.links.slice(),
 });
 
-const addTripleToGraph = (rdfGraph, source, subject, property, object) => {
-  addNodeIfNotExists(rdfGraph.nodes, source, subject);
+const addTripleToGraph = (rdfGraph, deltaRdfGrap, source, subject, property, object) => {
+  addNodeIfNotExists(rdfGraph.nodes, deltaRdfGrap, source, subject);
   if (property != null) {
     const triple = {
       subject,
       property,
       object,
     };
-    addLinkIfNotExists(rdfGraph, source, triple);
+    addLinkIfNotExists(rdfGraph, deltaRdfGrap, source, triple);
   }
 };
 
-const addLinkIfNotExists = (rdfGraph, source, triple) => {
+const addLinkIfNotExists = (rdfGraph, deltaRdfGrap, source, triple) => {
   let link = findLink(triple, rdfGraph.links);
   if (link == null) {
-    if (triple.property === 'http://www.w3.org/2000/01/rdf-schema#label') {
-      const node = addNodeIfNotExists(rdfGraph.nodes, source, triple.subject);
-      node.label = triple.object;
-      return null;
-    } else if (triple.property === 'http://www.w3.org/2000/01/rdf-schema#comment') {
-      const node = addNodeIfNotExists(rdfGraph.nodes, source, triple.subject);
-      node.comment = triple.object;
-      return null;
-    } else {
-      addNodeIfNotExists(rdfGraph.nodes, source, triple.object);
-      link = {
-        source: triple.subject,
-        target: triple.object,
-        property: triple.property
-      };
-      rdfGraph.links.push(link);
-    }
+    addNodeIfNotExists(rdfGraph.nodes, deltaRdfGrap, source, triple.object);
+    link = {
+      source: triple.subject,
+      target: triple.object,
+      property: triple.property,
+    };
+    rdfGraph.links.push(link);
+    deltaRdfGrap.links.push(link);
   }
+
   return link;
 };
 
-const addNodeIfNotExists = (nodes, source, subject) => {
+const addNodeIfNotExists = (nodes, deltaRdfGrap, source, subject) => {
   let node = findNode(subject, nodes);
   if (node != null) {
     return node;
@@ -110,6 +116,7 @@ const addNodeIfNotExists = (nodes, source, subject) => {
       bron: source,
     };
     nodes.push(node);
+    deltaRdfGrap.nodes.push(node);
   }
 
   return node;
