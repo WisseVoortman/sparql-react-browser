69614af38ab3414e75ac84a5182efad8e1c80045
Wisse Voortman <w_voortman@hotmail.com>
2020-11-03 09:53:07 +0100

    Updates

---
+++ precache-manifest.09061abd942e9df77c97f3ff311eee88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed2e2ec2d7c739f21c5b2d12d04d0a9",
+    "url": "/sparql-react-browser/index.html"
+  },
+  {
+    "revision": "79585b3126df1848bbef",
+    "url": "/sparql-react-browser/static/css/main.cbbf4d9c.chunk.css"
+  },
+  {
+    "revision": "544d3ae2d5178534fb98",
+    "url": "/sparql-react-browser/static/js/2.5a72ba20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sparql-react-browser/static/js/2.5a72ba20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79585b3126df1848bbef",
+    "url": "/sparql-react-browser/static/js/main.efbd7211.chunk.js"
+  },
+  {
+    "revision": "6e45e6a6286fd13fba83",
+    "url": "/sparql-react-browser/static/js/runtime-main.02568383.js"
+  }
+]);
